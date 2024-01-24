339c6195c101959929f6331e13fe6d0a4d3af58c
mathlcn <mathlcn0@gmail.com>
2024-01-24 11:45:07 -0300

    colocando views em estoque e venda

--- tlnsite/core/views.py
+++ tlnsite/core/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('core/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
